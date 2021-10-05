13df7d7f0681f9a9f4be8292ff32fa69c340b733
15629697282 <89985366+15629697282@users.noreply.github.com>
2021-10-05 22:43:35 +0800

    Add files via upload

---
+++ 10.2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	printf("ÐÕÃû£º»ÆÐñ\n");
+	printf("Ñ§ºÅ£º20211106067\n");
+	printf("* * * * * * * * *\n");
+	printf("* * * * * * * * *\n");
+	printf("* * * * * * * * *\n");
+	printf("* * * * * * * * *\n");
+	printf("* * * * * * * * *\n");
+	printf("* * * * * * * * *\n");
+	printf("* * * * * * * * *\n");
+	printf("* * * * * * * * *\n");
+	printf("* * * * * * * * *\n");
+	return 0;
+}
